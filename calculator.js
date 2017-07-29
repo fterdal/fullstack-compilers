a1eb8bca6fb146cbba3c8bddd4f992ddf530ef4e
Finn Terdal <fterdal@gmail.com>
2017-07-29 14:57:34 -0500

    add some of the parse functions

--- calculator.js
+++ calculator.js
@@ -41,13 +41,77 @@ Calculator.prototype.lexer = function(inputString) {
 }
 
 Calculator.prototype.peek = function() {
-  return this.tokenStream[0];
+  return this.tokenStream[0] || null;
 }
 
 Calculator.prototype.get = function() {
   return this.tokenStream.shift();
 }
 
-let calc = new Calculator("(11+2)*3");
-console.log(calc.peek());
-console.log(calc.tokenStream);
+function TreeNode(name){
+  this.name = name;
+  this.children = arguments;
+}
+
+Calculator.prototype.parseExpression = function(){
+    var term = this.parseTerm();
+    var a = this.parseA();
+
+    return new TreeNode("Expression", term, a);
+
+}
+
+Calculator.prototype.parseTerm = function(){
+  var factor = this.parseFactor();
+  var b = this.parseB();
+
+  return TreeNode("Term", factor, b);
+}
+
+Calculator.prototype.parseFactor = function(){
+//get the token stream
+  var token = this.peek();
+  if (token.name === "LPAREN") {
+    this.get();
+    return TreeNode("Factor", "(", this.parseExpression(), ")"); // TODO: Figure out what to do with ( E )
+  } else if (token.name === "SUB") {
+    this.get();
+    return TreeNode("Factor", "-", this.parseFactor());
+  } else if (token.name === "NUMBER") {
+    this.get();
+    return TreeNode("NUMBER", token.value);
+  }
+  throw new Error("Invalid Token");
+}
+
+Calculator.prototype.parseA = function(){
+  var nextToken = this.peek();
+  if(nextToken && nextToken.name === "ADD") {
+    this.get();
+    return new TreeNode("A", "+", this.parseTerm(), this.parseA());
+  } else if(nextToken && nextToken.name == "SUB") {
+    this.get();
+    return new TreeNode("A", "-", this.parseTerm(), this.parseA());
+  } else {
+      return new TreeNode("A")
+  }
+}
+
+// TODO
+Calculator.prototype.parseB = function(){
+  var nextToken = this.peek();
+  if(nextToken && nextToken.name === "MUL") {
+    this.get();
+    return new TreeNode("B", "*", this.parseTerm(), this.parseB());
+  } else if(nextToken && nextToken.name == "DIV") {
+    this.get();
+    return new TreeNode("B", "/", this.parseTerm(), this.parseB());
+  } else {
+    return new TreeNode("B")
+  }
+}
+
+let calc = new Calculator("1");
+console.dir( calc.parseExpression() );
+//console.log(calc.peek());
+// console.log(calc.tokenStream);
